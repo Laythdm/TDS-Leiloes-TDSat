9ec917f3970aeef51089c0908d5b04c59f95a24c
Layth <braulu1234@gmail.com+>
2024-01-15 17:01:35 -0300

    Alterado o metodo de cadastro de produtos para registro no banco de dados

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -12,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -24,9 +25,29 @@ public class ProdutosDAO {
     
     public void cadastrarProduto (ProdutosDTO produto){
         
-        
-        //conn = new conectaDAO().connectDB();
-        
+        try {
+
+            conectaDAO db = new  conectaDAO();
+            db.connectDB();
+            
+            
+
+            String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+            PreparedStatement consulta = db.connectDB().prepareStatement(sql);
+            consulta.setString(1, produto.getNome());
+            consulta.setInt(2, produto.getValor());
+            consulta.setString(3, produto.getStatus());
+           
+            
+            
+
+            consulta.execute();
+            System.out.println("cadastrado com sucesso");
+
+            
+        } catch (SQLException e) {
+            System.out.println("erro ao cadastrar");
+        }
         
     }
     
